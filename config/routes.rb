13de3445b766f5a8976d6d44db08bb7e17ad9e6e
Nurbolat Kusmagul <argnur@gmail.com>
2014-12-02 11:13:46 +0000

    last chapter

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Twitter::Application.routes.draw do
+  
+  resources :users
+  
   match "/help", to: "static_pages#help", via: :get
   match "/contact", to: "static_pages#contact", via: :get
   match "/about", to: "static_pages#about", via: :get
